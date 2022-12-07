ca23c145c757f34a26f705b1af25516e78bd4eff
Aleph Perth <95363770+AlephPerth@users.noreply.github.com>
2022-12-07 16:19:03 -0300

    Add files via upload

--- Proyecto 1 - Peso en otro planeta/pesoPlaneta.js
+++ Proyecto 1 - Peso en otro planeta/pesoPlaneta.js
@@ -10,18 +10,24 @@ const g_luna = 1.62;
 const g_sol = 274;
 
 const g_tierra = 9.8;
-let f_gravedad = 0
+let f_gravedad = 0;
 let resultado = null;
 let peso_final = null;
 let usuario = null;
-let planeta = document.getElementsByName('planeta').value
 
 const calcular = document.getElementById('calcular')
     calcular.addEventListener('click',gravedad)
 
-const mercurio = document.getElementById('mercurio')
- 
 function gravedad () {
+
+    let planeta = document.getElementsByName('planeta')
+
+    for (let i = 0; i < planeta.length; i++) {
+        if (planeta [i].checked) {
+            console.log(planeta[i].id)
+        } 
+    }
+    
         usuario = document.getElementById('usuario').value
         f_gravedad = 274
     
