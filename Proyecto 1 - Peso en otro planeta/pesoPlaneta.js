6bbe04906d673578305f04fb27ce78e19c364883
Aleph Perth <arcanforce00@gmail.com>
2022-12-19 17:24:43 -0300

    separo archivo html

--- Proyecto 1 - Peso en otro planeta/pesoPlaneta.js
+++ Proyecto 1 - Peso en otro planeta/pesoPlaneta.js
@@ -1,14 +1,23 @@
 const g_tierra = 9.8;
 let f_gravedad = 0;
-let nombre_planeta= null;
+let nombre_planeta = null;
 let resultado = null;
 let peso_final = null;
-let usuario = null;
+let peso_usuario = null;
+
+let siguiente = document.getElementById('siguiente')
+siguiente.addEventListener('click',p_usuario)
+
+function p_usuario () {
+    peso_usuario = document.getElementById('peso_usuario').value
+}
 
 const calcular = document.getElementById('calcular')
 calcular.addEventListener('click',gravedad)
 
 function gravedad () {
+
+    peso_usuario
 
     let planeta = document.getElementsByName('planeta')
 
@@ -24,16 +33,15 @@ function gravedad () {
             f_gravedad = array[i].value
         } 
     }
-        usuario = document.getElementById('usuario').value
 
-    if (usuario > 0 && f_gravedad > 0) {
-        peso_final = parseInt(usuario * f_gravedad / g_tierra)
+    if (peso_usuario > 0 && f_gravedad > 0) {
+        peso_final = parseInt(peso_usuario * f_gravedad / g_tierra)
 
     resultado = 
         `<h2> Tu peso en ${nombre_planeta} seria de: ${peso_final} Kg</h2>`
 
         document.getElementById('resultado').innerHTML = resultado
-  
+
         } else {
             alert ('ERROR!')
         }
