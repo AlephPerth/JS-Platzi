bbe64e75a93476e96818ed2ce59cb34a1b395ba8
Aleph Perth <arcanforce00@gmail.com>
2022-12-27 15:46:00 -0300

    update datos objeto Planets

--- Proyecto 1 - Peso en otro planeta/JS/pesoPlaneta.js
+++ Proyecto 1 - Peso en otro planeta/JS/pesoPlaneta.js
@@ -20,14 +20,14 @@ class Planets {
     }
 }
 
-const mercurio = new Planets ('mercurio', './img/Mercurio.jpg')
-const venus = new Planets ('venus', './img/Venus.jpg')
-const marte = new Planets ('marte', './img/Marte.jpg')
-const jupiter = new Planets ('jupiter', './img/Jupiter.jpg')
-const saturno = new Planets ('saturno', './img/Saturno.jpg')
-const urano = new Planets ('urano', './img/Urano.jpg')
-const neptuno = new Planets ('neptuno', './img/Neptuno.jpg')
-const pluton = new Planets ('pluton', './img/Pluton.jpg')
+const mercurio = new Planets ('mercurio', './img/Mercurio.jpg', '58,7 días', '57.894.376KM','3.7 m/s' )
+const venus = new Planets ('venus', './img/Venus.jpg', '243,0187 días', '6051.8KM', '8.87 m/s')
+const marte = new Planets ('marte', './img/Marte.jpg', '24h 39min 35s', '3389.5KM', '3.7 m/s')
+const jupiter = new Planets ('jupiter', './img/Jupiter.jpg', '9h 55m 30s', '71492.0KM', '24.8 m/s')
+const saturno = new Planets ('saturno', './img/Saturno.jpg', '10h 33m 38s', '58232.0KM', '10.44 m/s')
+const urano = new Planets ('urano', './img/Urano.jpg', '-17h 14m', '51.118KM', '8.87 m/s')
+const neptuno = new Planets ('neptuno', './img/Neptuno.jpg', '16h 6m 14s', '24622KM', '11.15 m/s')
+const pluton = new Planets ('pluton', './img/Pluton.jpg', '153hs','1185KM','0.62 m/s')
 const luna = new Planets ('la luna', './img/Luna.jpg', '27d 7h 43,7min', '384.403KM', '1.62 m/s')
 
 planets_data.push(mercurio, venus, marte, jupiter, saturno, urano, neptuno, pluton, luna)
@@ -77,11 +77,11 @@ function gravity () {
         document.getElementById("section-B").style.display = "none"
         document.getElementById("section-C").style.display = "block"
 
-        //console.log(final_weight)
+//console.log(final_weight)
 
         result = 
     
-        `<p>En ${luna.name} los dias duran ${luna.rotation} 
+        `<p> En ${luna.name} los dias duran ${luna.rotation} 
         y su radio es de ${luna.radio}. La fuerza de gravedad en 
         ${planet_name} es de ${luna.gravity}</p> 
         <img class= img_planet src = ${luna.picture}>
